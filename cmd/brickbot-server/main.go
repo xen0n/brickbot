850956685fe0c194ce631d847d2e285650e508d6
WANG Xuerui <git@xen0n.name>
2021-07-09 14:46:08 +0800

    feat: add barebones HTTP server

--- cmd/brickbot-server/main.go
+++ cmd/brickbot-server/main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -25,5 +26,24 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(conf)
+	err = runServer(&conf)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func runServer(conf *config) error {
+	mux := http.NewServeMux()
+
+	// Health check endpoints.
+	mux.HandleFunc("/healthz", dummyHealthzHandler)
+	mux.HandleFunc("/livez", dummyHealthzHandler)
+	mux.HandleFunc("/readyz", dummyHealthzHandler)
+
+	return http.ListenAndServe(conf.Server.ListenAddr, mux)
+}
+
+func dummyHealthzHandler(rw http.ResponseWriter, r *http.Request) {
+	// Does nothing for now, just report healthy.
+	rw.WriteHeader(http.StatusOK)
 }
